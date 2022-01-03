9352d852c344e08a209b0b56e01afad53f0ca761
Guevaras1explanations <85652292+Guevaras1explanations@users.noreply.github.com>
2022-01-03 03:36:57 +0300

    Add files via upload

---
+++ userbot/__init__.py
@@ -0,0 +1,63 @@
+import time
+import heroku3
+from .Config import Config
+from .core.logger import logging
+from .core.session import iqthon
+from .sql_helper.globals import addgvar, delgvar, gvarstatus
+__version__ = "7.3"
+__license__ = "GNU Affero General Public License v3.0"
+__author__ = "<t.me/E999G>"
+__copyright__ = "telethon AR (C) 2020 - 2021  " + __author__
+iqthon.version = __version__
+iqthon.tgbot.version = __version__
+LOGS = logging.getLogger("IQTHON")
+bot = iqthon
+StartTime = time.time()
+catversion = "7.2"
+if Config.UPSTREAM_REPO == "Telethon-Ar":
+    UPSTREAM_REPO_URL = "https://github.com/Guevaras1explanations/TelethonArab-"
+else:
+    UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
+if Config.PRIVATE_GROUP_BOT_API_ID == 0:
+    if gvarstatus("PRIVATE_GROUP_BOT_API_ID") is None:
+        Config.BOTLOG = False
+        Config.BOTLOG_CHATID = "me"
+    else:
+        Config.BOTLOG_CHATID = int(gvarstatus("PRIVATE_GROUP_BOT_API_ID"))
+        Config.PRIVATE_GROUP_BOT_API_ID = int(gvarstatus("PRIVATE_GROUP_BOT_API_ID"))
+        Config.BOTLOG = True
+else:
+    if str(Config.PRIVATE_GROUP_BOT_API_ID)[0] != "-":
+        Config.BOTLOG_CHATID = int("-" + str(Config.PRIVATE_GROUP_BOT_API_ID))
+    else:
+        Config.BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
+    Config.BOTLOG = True
+if Config.PM_LOGGER_GROUP_ID == 0:
+    if gvarstatus("PM_LOGGER_GROUP_ID") is None:
+        Config.PM_LOGGER_GROUP_ID = -100
+    else:
+        Config.PM_LOGGER_GROUP_ID = int(gvarstatus("PM_LOGGER_GROUP_ID"))
+elif str(Config.PM_LOGGER_GROUP_ID)[0] != "-":
+    Config.PM_LOGGER_GROUP_ID = int("-" + str(Config.PM_LOGGER_GROUP_ID))
+
+try:
+    if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:
+        HEROKU_APP = heroku3.from_key(Config.HEROKU_API_KEY).apps()[Config.HEROKU_APP_NAME]
+    else:
+        HEROKU_APP = None
+except Exception:
+    HEROKU_APP = None
+COUNT_MSG = 0
+ISAFK = False
+AFKREASON = None
+USERS = {}
+COUNT_PM = {}
+LASTMSG = {}
+CMD_HELP = {}
+CMD_LIST = {}
+SUDO_LIST = {}
+LOAD_PLUG = {}
+INT_PLUG = ""
+BOTLOG = Config.BOTLOG
+BOTLOG_CHATID = Config.BOTLOG_CHATID
+PM_LOGGER_GROUP_ID = Config.PM_LOGGER_GROUP_ID
---
+++ userbot/__main__.py
@@ -0,0 +1,85 @@
+import sys
+import userbot
+from userbot import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
+from telethon import functions
+from .Config import Config
+from .core.logger import logging
+from .core.session import iqthon
+from .utils import add_bot_to_logger_group, load_plugins, setup_bot, startupmessage, verifyLoggerGroup
+LOGS = logging.getLogger(
+"تليثون العرب"
+)
+print(
+userbot.__copyright__)
+print(
+"المرخصة بموجب شروط " + userbot.__license__)
+cmdhr = Config.COMMAND_HAND_LER
+try:
+    LOGS.info(
+"بدء تنزيل تليثون العرب"
+)
+    iqthon.loop.run_until_complete(
+setup_bot())
+    LOGS.info("بدء تشغيل البوت")
+except Exception as e:
+    LOGS.error(
+f"{str(e)}")
+    sys.exit()
+class CatCheck:
+    def __init__(self):
+        self.sucess = True
+Catcheck = CatCheck()
+async def startup_process():
+    await verifyLoggerGroup()
+    await load_plugins("plugins")
+    await load_plugins("assistant")
+    print(
+f"<b> ⌔︙ اهلا بك لقد نصبت تليثون العرب بنجاح 🥁 اذهب الى قناتنا لمعرفة المزيـد ⤵️. </b>\n CH : https://t.me/E999G "
+)
+    await verifyLoggerGroup()
+    await add_bot_to_logger_group(BOTLOG_CHATID)
+    if PM_LOGGER_GROUP_ID != -100:
+        await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
+    await startupmessage()
+    Catcheck.sucess = True
+    return
+iqthon.loop.run_until_complete(startup_process())
+def start_bot():
+  try:
+    iqthon.loop.run_until_complete(
+iqthon(
+functions.channels.JoinChannelRequest
+(
+"IQTHON"
+))
+)
+    iqthon.loop.run_until_complete(
+iqthon(
+functions.channels.JoinChannelRequest("TelethonMusic"
+)
+))
+    iqthon.loop.run_until_complete(
+iqthon(
+functions.channels.JoinChannelRequest(
+"E999G"
+)))
+  except Exception as e:
+    print(e)
+    return False
+Checker = start_bot()
+if Checker == False:
+    print(
+"عذرا لديك حظر مؤقت حاول التنصيب غدا او بعد 24 ساعة"
+)
+    iqthon.disconnect()
+    sys.exit()
+if len(sys.argv) not in (1, 3, 4):
+    iqthon.disconnect()
+elif not Catcheck.sucess:
+    if HEROKU_APP is not None:
+        HEROKU_APP.restart()
+else:
+    try:
+        iqthon.run_until_disconnected()
+    except ConnectionError:
+        pass

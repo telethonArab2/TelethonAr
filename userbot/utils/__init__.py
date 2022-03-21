69302b9c83be512f1bd41deea22ecdf3e830257c
telethonArab <102070450+telethonArab@users.noreply.github.com>
2022-03-21 23:55:43 +0300

    Add files via upload

---
+++ userbot/utils/__init__.py
@@ -0,0 +1,4 @@
+from ..helpers.progress import *
+from .decorators import *
+from .klanr import *
+from .startup import *
---
+++ userbot/utils/decorators.py
@@ -0,0 +1,368 @@
+import datetime
+import inspect
+import re
+import sys
+import traceback
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
+from pathlib import Path
+from .. import CMD_LIST, LOAD_PLUG, SUDO_LIST
+from ..Config import Config
+from ..core.data import _sudousers_list, blacklist_chats_list
+from ..core.events import MessageEdited, NewMessage
+from ..core.logger import logging
+from ..core.session import iqthon
+from ..helpers.utils.format import paste_message
+from ..helpers.utils.utils import runcmd
+from ..sql_helper.globals import gvarstatus
+
+LOGS = logging.getLogger(__name__)
+
+
+async def is_admin(catub, chat_id, userid):
+    if not str(chat_id).startswith("-100"):
+        return False
+    try:
+        req_jo = await catub(GetParticipantRequest(chat_id, userid))
+        chat_participant = req_jo.participant
+        if isinstance(
+            chat_participant, (ChannelParticipantCreator, ChannelParticipantAdmin)
+        ):
+            return True
+    except Exception as e:
+        LOGS.info(str(e))
+        return False
+    else:
+        return False
+
+
+def admin_cmd(pattern=None, command=None, **args):  # sourcery no-metrics
+    args["func"] = lambda e: e.via_bot_id is None
+    stack = inspect.stack()
+    previous_stack_frame = stack[1]
+    file_test = Path(previous_stack_frame.filename)
+    file_test = file_test.stem.replace(".py", "")
+    allow_sudo = args.get("allow_sudo", False)
+    if pattern is not None:
+        if pattern.startswith(r"\#"):
+            args["pattern"] = re.compile(pattern)
+        elif pattern.startswith(r"^"):
+            args["pattern"] = re.compile(pattern)
+            cmd = pattern.replace("$", "").replace("^", "").replace("\\", "")
+            try:
+                CMD_LIST[file_test].append(cmd)
+            except BaseException:
+                CMD_LIST.update({file_test: [cmd]})
+        else:
+            if len(Config.COMMAND_HAND_LER) == 2:
+                catreg = "^" + Config.COMMAND_HAND_LER
+                reg = Config.COMMAND_HAND_LER[1]
+            elif len(Config.COMMAND_HAND_LER) == 1:
+                catreg = "^\\" + Config.COMMAND_HAND_LER
+                reg = Config.COMMAND_HAND_LER
+            args["pattern"] = re.compile(catreg + pattern)
+            if command is not None:
+                cmd = reg + command
+            else:
+                cmd = (
+                    (reg + pattern).replace("$", "").replace("\\", "").replace("^", "")
+                )
+            try:
+                CMD_LIST[file_test].append(cmd)
+            except BaseException:
+                CMD_LIST.update({file_test: [cmd]})
+    args["outgoing"] = True
+    if allow_sudo:
+        args["from_users"] = list(Config.SUDO_USERS)
+        args["incoming"] = True
+        del args["allow_sudo"]
+    elif "incoming" in args and not args["incoming"]:
+        args["outgoing"] = True
+    if gvarstatus("blacklist_chats") is not None:
+        args["blacklist_chats"] = True
+        args["chats"] = blacklist_chats_list()
+    if "allow_edited_updates" in args and args["allow_edited_updates"]:
+        del args["allow_edited_updates"]
+    return NewMessage(**args)
+
+
+async def eor(
+    event,
+    text,
+    parse_mode=None,
+    link_preview=None,
+    file_name=None,
+    aslink=False,
+    linktext=None,
+    caption=None,
+):
+    link_preview = link_preview or False
+    reply_to = await event.get_reply_message()
+    if len(text) < 4096:
+        parse_mode = parse_mode or "md"
+        if event.sender_id in Config.SUDO_USERS:
+            if reply_to:
+                return await reply_to.reply(
+                    text, parse_mode=parse_mode, link_preview=link_preview
+                )
+            return await event.reply(
+                text, parse_mode=parse_mode, link_preview=link_preview
+            )
+        return await event.edit(text, parse_mode=parse_mode, link_preview=link_preview)
+    asciich = ["*", "`", "_"]
+    for i in asciich:
+        text = re.sub(rf"\{i}", "", text)
+    if aslink:
+        linktext = linktext or "Message was to big so pasted to bin"
+        try:
+            key = (
+                requests.post(
+                    "https://nekobin.com/api/documents", json={"content": text}
+                )
+                .json()
+                .get("result")
+                .get("key")
+            )
+            text = linktext + f" [here](https://nekobin.com/{key})"
+        except:
+            text = re.sub(r"•", ">>", text)
+            kresult = requests.post(
+                "https://del.dog/documents", data=text.encode("UTF-8")
+            ).json()
+            text = linktext + f" [here](https://del.dog/{kresult['key']})"
+        if event.sender_id in Config.SUDO_USERS:
+            if reply_to:
+                return await reply_to.reply(text, link_preview=link_preview)
+            return await event.reply(text, link_preview=link_preview)
+        return await event.edit(text, link_preview=link_preview)
+    file_name = file_name or "output.txt"
+    caption = caption or None
+    with open(file_name, "w+") as output:
+        output.write(text)
+    if reply_to:
+        await reply_to.reply(caption, file=file_name)
+        await event.delete()
+        return os.remove(file_name)
+    if event.sender_id in Config.SUDO_USERS:
+        await event.reply(caption, file=file_name)
+        await event.delete()
+        return os.remove(file_name)
+    await event.client.send_file(event.chat_id, file_name, caption=caption)
+    await event.delete()
+    os.remove(file_name)
+
+
+
+
+def sudo_cmd(pattern=None, command=None, **args):  # sourcery no-metrics
+    args["func"] = lambda e: e.via_bot_id is None
+    stack = inspect.stack()
+    previous_stack_frame = stack[1]
+    file_test = Path(previous_stack_frame.filename)
+    file_test = file_test.stem.replace(".py", "")
+    allow_sudo = args.get("allow_sudo", False)
+    # get the pattern from the decorator
+    if pattern is not None:
+        if pattern.startswith(r"\#"):
+            # special fix for snip.py
+            args["pattern"] = re.compile(pattern)
+        elif pattern.startswith(r"^"):
+            args["pattern"] = re.compile(pattern)
+            cmd = pattern.replace("$", "").replace("^", "").replace("\\", "")
+            try:
+                SUDO_LIST[file_test].append(cmd)
+            except BaseException:
+                SUDO_LIST.update({file_test: [cmd]})
+        else:
+            if len(Config.SUDO_COMMAND_HAND_LER) == 2:
+                catreg = "^" + Config.SUDO_COMMAND_HAND_LER
+                reg = Config.SUDO_COMMAND_HAND_LER[1]
+            elif len(Config.SUDO_COMMAND_HAND_LER) == 1:
+                catreg = "^\\" + Config.SUDO_COMMAND_HAND_LER
+                reg = Config.COMMAND_HAND_LER
+            args["pattern"] = re.compile(catreg + pattern)
+            if command is not None:
+                cmd = reg + command
+            else:
+                cmd = (
+                    (reg + pattern).replace("$", "").replace("\\", "").replace("^", "")
+                )
+            try:
+                SUDO_LIST[file_test].append(cmd)
+            except BaseException:
+                SUDO_LIST.update({file_test: [cmd]})
+    args["outgoing"] = True
+    # should this command be available for other users?
+    if allow_sudo:
+        args["from_users"] = list(_sudousers_list())
+        # Mutually exclusive with outgoing (can only set one of either).
+        args["incoming"] = True
+        del args["allow_sudo"]
+    # error handling condition check
+    elif "incoming" in args and not args["incoming"]:
+        args["outgoing"] = True
+    # add blacklist chats, UB should not respond in these chats
+    if gvarstatus("blacklist_chats") is not None:
+        args["blacklist_chats"] = True
+        args["chats"] = blacklist_chats_list()
+    # add blacklist chats, UB should not respond in these chats
+    if "allow_edited_updates" in args and args["allow_edited_updates"]:
+        del args["allow_edited_updates"]
+    # check if the plugin should listen for outgoing 'messages'
+    if gvarstatus("sudoenable") is not None:
+        return NewMessage(**args)
+
+
+def errors_handler(func):
+    async def wrapper(errors):
+        try:
+            await func(errors)
+        except BaseException:
+            if Config.PRIVATE_GROUP_BOT_API_ID != 0:
+                return
+            date = (datetime.datetime.now()).strftime("%m/%d/%Y, %H:%M:%S")
+            ftext = f"\n--------BEGIN USERBOT TRACEBACK LOG--------\
+                                  \nDate: {date}\nGroup ID: {str(check.chat_id)}\
+                                  \nSender ID: {str(check.sender_id)}\
+                                  \n\nEvent Trigger:\n{str(check.text)}\
+                                  \n\nTraceback info:\n{str(traceback.format_exc())}\
+                                  \n\nError text:\n{str(sys.exc_info()[1])}"
+            new = {
+                "error": str(sys.exc_info()[1]),
+                "date": datetime.datetime.now(),
+            }
+
+            ftext += "\n\n--------END USERBOT TRACEBACK LOG--------"
+            command = 'git log --pretty=format:"%an: %s" -5'
+            ftext += "\n\n\nLast 5 commits:\n"
+            output = (await runcmd(command))[:2]
+            result = output[0] + output[1]
+            ftext += result
+            pastelink = await paste_message(ftext)
+            text = "**iqthon**\n\n"
+            text += "If you wanna you can report it"
+            text += f"- just forward this message {link}.\n"
+            text += "Nothing is logged except the fact of error and date\n\n"
+            text += f"**Error report : ** [{new['error']}]({pastelink})"
+            await check.client.send_message(
+                Config.PRIVATE_GROUP_BOT_API_ID, text, link_preview=False
+            )
+
+    return wrapper
+
+
+def register(**args):
+    args["func"] = lambda e: e.via_bot_id is None
+    stack = inspect.stack()
+    previous_stack_frame = stack[1]
+    file_test = Path(previous_stack_frame.filename)
+    file_test = file_test.stem.replace(".py", "")
+    pattern = args.get("pattern", None)
+    disable_edited = args.get("disable_edited", True)
+    allow_sudo = args.get("allow_sudo", False)
+
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+
+    if "disable_edited" in args:
+        del args["disable_edited"]
+
+    reg = re.compile("(.*)")
+    if pattern is not None:
+        try:
+            cmd = re.search(reg, pattern)
+            try:
+                cmd = cmd.group(1).replace("$", "").replace("\\", "").replace("^", "")
+            except BaseException:
+                pass
+
+            try:
+                CMD_LIST[file_test].append(cmd)
+            except BaseException:
+                CMD_LIST.update({file_test: [cmd]})
+        except BaseException:
+            pass
+
+    if allow_sudo:
+        args["from_users"] = list(Config.SUDO_USERS)
+        # Mutually exclusive with outgoing (can only set one of either).
+        args["incoming"] = True
+        del args["allow_sudo"]
+
+    # error handling condition check
+    elif "incoming" in args and not args["incoming"]:
+        args["outgoing"] = True
+
+    # add blacklist chats, UB should not respond in these chats
+    if gvarstatus("blacklist_chats") is not None:
+        args["blacklist_chats"] = True
+        args["chats"] = blacklist_chats_list()
+
+    def decorator(func):
+        if not disable_edited:
+            iqthon.add_event_handler(func, MessageEdited(**args))
+        iqthon.add_event_handler(func, NewMessage(**args))
+        try:
+            LOAD_PLUG[file_test].append(func)
+        except Exception:
+            LOAD_PLUG.update({file_test: [func]})
+        return func
+
+    return decorator
+
+
+def command(**args):
+    args["func"] = lambda e: e.via_bot_id is None
+    stack = inspect.stack()
+    previous_stack_frame = stack[1]
+    file_test = Path(previous_stack_frame.filename)
+    file_test = file_test.stem.replace(".py", "")
+    pattern = args.get("pattern", None)
+    allow_sudo = args.get("allow_sudo", None)
+    allow_edited_updates = args.get("allow_edited_updates", False)
+    args["incoming"] = args.get("incoming", False)
+    args["outgoing"] = True
+    if bool(args["incoming"]):
+        args["outgoing"] = False
+    try:
+        if pattern is not None and not pattern.startswith("(?i)"):
+            args["pattern"] = "(?i)" + pattern
+    except BaseException:
+        pass
+    reg = re.compile("(.*)")
+    if pattern is not None:
+        try:
+            cmd = re.search(reg, pattern)
+            try:
+                cmd = cmd.group(1).replace("$", "").replace("\\", "").replace("^", "")
+            except BaseException:
+                pass
+            try:
+                CMD_LIST[file_test].append(cmd)
+            except BaseException:
+                CMD_LIST.update({file_test: [cmd]})
+        except BaseException:
+            pass
+    if allow_sudo:
+        args["from_users"] = list(Config.SUDO_USERS)
+        args["incoming"] = True
+    del allow_sudo
+    try:
+        del args["allow_sudo"]
+    except BaseException:
+        pass
+    if gvarstatus("blacklist_chats") is not None:
+        args["blacklist_chats"] = True
+        args["chats"] = blacklist_chats_list()
+
+    def decorator(func):
+        if allow_edited_updates:
+            iqthon.add_event_handler(func, MessageEdited(**args))
+        iqthon.add_event_handler(func, NewMessage(**args))
+        try:
+            LOAD_PLUG[file_test].append(func)
+        except BaseException:
+            LOAD_PLUG.update({file_test: [func]})
+        return func
+
+    return decorator

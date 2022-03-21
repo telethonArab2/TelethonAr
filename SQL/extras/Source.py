0a3445c34827bc2dbc16ce0818eca8cf140293db
telethonArab <102070450+telethonArab@users.noreply.github.com>
2022-03-21 23:33:00 +0300

    Delete Source.py

--- SQL/extras/Source.py
+++
@@ -1,11 +0,0 @@
-import re
-import time
-from datetime import datetime
-from userbot import StartTime, iqthon
-from userbot.Config import Config
-from userbot.plugins import mention
-help1 = ("**⎈ ⦙ كيفيه التنصيب :**")
-help2 = ("**⎈ ⦙ قـائمـه الاوامـر :**\n**⎈ ⦙ قنـاه السـورس :** @E999G\n**⎈ ⦙ شـرح اوامـر السـورس : @X888E**\n**⎈ ⦙ شـرح فـارات السـورس : @E999G** ")
-TG_BOT = Config.TG_BOT_USERNAME
-TM = time.strftime("%I:%M")
-Sour = f"**.𓄌 : version 7.3  𓇡.** \n.𓄌 : me  {

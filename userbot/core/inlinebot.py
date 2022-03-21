32861eeb62aa93dabcbd8b137770daf737172b97
telethonArab <102070450+telethonArab@users.noreply.github.com>
2022-03-21 23:52:24 +0300

    Add files via upload

---
+++ userbot/core/inlinebot.py
@@ -0,0 +1,341 @@
+import json
+import math
+import os
+import random
+import re
+import time
+from uuid import uuid4
+from platform import python_version
+from telethon import Button, types, version
+from telethon.errors import QueryIdInvalidError
+from telethon.events import CallbackQuery, InlineQuery
+from youtubesearchpython import VideosSearch
+from userbot import iqthon, catversion, StartTime
+from ..Config import Config
+from ..helpers.functions import rand_key, catalive, check_data_base_heal_th, get_readable_time
+from ..helpers.functions.utube import download_button, get_yt_video_id, get_ytthumb, result_formatter, ytsearch_data
+from ..plugins import mention
+from ..sql_helper.globals import gvarstatus
+from . import CMD_INFO, GRP_INFO, PLG_INFO, check_owner
+from .logger import logging
+LOGS = logging.getLogger(__name__)
+BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)\]\<buttonurl:(?:/{0,2})(.+?)(:same)?\>)")
+CATLOGO = "https://telegra.ph/file/1bf9c1b0a084c258b1f97.jpg"
+tr = Config.COMMAND_HAND_LER
+def getkey(val):
+    for key, value in GRP_INFO.items():
+        for plugin in value:
+            if val == plugin:
+                return key
+    return None
+
+def ibuild_keyboard(buttons):
+    keyb = []
+    for btn in buttons:
+        if btn[2] and keyb:
+            keyb[-1].append(Button.url(btn[0], btn[1]))
+        else:
+            keyb.append([Button.url(btn[0], btn[1])])
+    return keyb
+
+@iqthon.tgbot.on(InlineQuery)
+async def inline_handler(event):  
+    builder = event.builder
+    result = None
+    query = event.text
+    string = query.lower()
+    query.split(" ", 2)
+    str_y = query.split(" ", 1)
+    string.split()
+    query_user_id = event.query.user_id
+    if query_user_id == Config.OWNER_ID or query_user_id in Config.SUDO_USERS:
+        hmm = re.compile("همسه (.*) (.*)")
+        match = re.findall(hmm, query)
+        if query.startswith("**iqthonbot"):
+            buttons = [
+                (
+                    Button.inline("السورس", data="stats"),
+                    Button.url("الريبو", "https://github.com/telethon-Arab/telethonNow"),
+                )
+            ]
+            ALIVE_PIC = gvarstatus("ALIVE_PIC")
+            IALIVE_PIC = gvarstatus("IALIVE_PIC")
+            if IALIVE_PIC:
+                CAT = [x for x in IALIVE_PIC.split()]
+                PIC = list(CAT)
+                I_IMG = random.choice(PIC)
+            if not IALIVE_PIC and ALIVE_PIC:
+                CAT = [x for x in ALIVE_PIC.split()]
+                PIC = list(CAT)
+                I_IMG = random.choice(PIC)
+            elif not IALIVE_PIC:
+                I_IMG = None
+            if I_IMG and I_IMG.endswith((".jpg", ".png")):
+                result = builder.photo(
+                    I_IMG,
+                    text=query,
+                    buttons=buttons,
+                )
+            elif I_IMG:
+                result = builder.document(
+                    I_IMG,
+                    title="Alive cat",
+                    text=query,
+                    buttons=buttons,
+                )
+            else:
+                result = builder.article(
+                    title="Alive cat",
+                    text=query,
+                    buttons=buttons,
+                )
+            await event.answer([result] if result else None)
+        elif query.startswith("Inline buttons"):
+            markdown_note = query[14:]
+            prev = 0
+            note_data = ""
+            buttons = []
+            for match in BTN_URL_REGEX.finditer(markdown_note):
+                n_escapes = 0
+                to_check = match.start(1) - 1
+                while to_check > 0 and markdown_note[to_check] == "\\":
+                    n_escapes += 1
+                    to_check -= 1
+                if n_escapes % 2 == 0:
+                    buttons.append(
+                        (match.group(2), match.group(3), bool(match.group(4)))
+                    )
+                    note_data += markdown_note[prev : match.start(1)]
+                    prev = match.end(1)
+                elif n_escapes % 2 == 1:
+                    note_data += markdown_note[prev:to_check]
+                    prev = match.start(1) - 1
+                else:
+                    break
+            else:
+                note_data += markdown_note[prev:]
+            message_text = note_data.strip()
+            tl_ib_buttons = ibuild_keyboard(buttons)
+            result = builder.article(
+                title="Inline creator",
+                text=message_text,
+                buttons=tl_ib_buttons,
+                link_preview=False,
+            )
+            await event.answer([result] if result else None)
+        elif match:
+            query = query[7:]
+            user, txct = query.split(" ", 1)
+            builder = event.builder
+            secret = os.path.join("./userbot", "secrets.txt")
+            try:
+                jsondata = json.load(open(secret))
+            except Exception:
+                jsondata = False
+            try:
+                # if u is user id
+                u = int(user)
+                try:
+                    u = await event.client.get_entity(u)
+                    if u.username:
+                        sandy = f"@{u.username}"
+                    else:
+                        sandy = f"[{u.first_name}](tg://user?id={u.id})"
+                except ValueError:
+                    sandy = f"[user](tg://user?id={u})"
+            except ValueError:
+                try:
+                    u = await event.client.get_entity(user)
+                except ValueError:
+                    return
+                if u.username:
+                    sandy = f"@{u.username}"
+                else:
+                    sandy = f"[{u.first_name}](tg://user?id={u.id})"
+                u = int(u.id)
+            except Exception:
+                return
+            timestamp = int(time.time() * 2)
+            newsecret = {str(timestamp): {"userid": u, "text": txct}}
+
+            buttons = [Button.inline("اظهر الرسالة 🔐", data=f"secret_{timestamp}")]
+            result = builder.article(
+                title="رسالة سرية",
+                text=f"🔒 رسالة تهمس ل {sandy}, هو فقط من يمكنه فتحه.",
+                buttons=buttons,
+            )
+            await event.answer([result] if result else None)
+            if jsondata:
+                jsondata.update(newsecret)
+                json.dump(jsondata, open(secret, "w"))
+            else:
+                json.dump(newsecret, open(secret, "w"))
+        elif str_y[0].lower() == "ytdl" and len(str_y) == 2:
+            link = get_yt_video_id(str_y[1].strip())
+            found_ = True
+            if link is None:
+                search = VideosSearch(str_y[1].strip(), limit=15)
+                resp = (search.result()).get("result")
+                if len(resp) == 0:
+                    found_ = False
+                else:
+                    outdata = await result_formatter(resp)
+                    key_ = rand_key()
+                    ytsearch_data.store_(key_, outdata)
+                    buttons = [
+                        Button.inline(
+                            f"1 / {len(outdata)}",
+                            data=f"ytdl_next_{key_}_1",
+                        ),
+                        Button.inline(
+                            "📜  List all",
+                            data=f"ytdl_listall_{key_}_1",
+                        ),
+                        Button.inline(
+                            "⬇️  Download",
+                            data=f'ytdl_download_{outdata[1]["video_id"]}_0',
+                        ),
+                    ]
+                    caption = outdata[1]["message"]
+                    photo = await get_ytthumb(outdata[1]["video_id"])
+            else:
+                caption, buttons = await download_button(link, body=True)
+                photo = await get_ytthumb(link)
+            if found_:
+                markup = event.client.build_reply_markup(buttons)
+                photo = types.InputWebDocument(
+                    url=photo, size=0, mime_type="image/jpeg", attributes=[]
+                )
+                text, msg_entities = await event.client._parse_message_text(
+                    caption, "html"
+                )
+                result = types.InputBotInlineResult(
+                    id=str(uuid4()),
+                    type="photo",
+                    title=link,
+                    description="⬇️ Click to Download",
+                    thumb=photo,
+                    content=photo,
+                    send_message=types.InputBotInlineMessageMediaAuto(
+                        reply_markup=markup, message=text, entities=msg_entities
+                    ),
+                )
+            else:
+                result = builder.article(
+                    title="Not Found",
+                    text=f"No Results found for `{str_y[1]}`",
+                    description="INVALID",
+                )
+            try:
+                await event.answer([result] if result else None)
+            except QueryIdInvalidError:
+                await event.answer(
+                    [
+                        builder.article(
+                            title="Not Found",
+                            text=f"No Results found for `{str_y[1]}`",
+                            description="INVALID",
+                        )
+                    ]
+                )
+        elif string == "age_verification_alert":
+            buttons = [
+                Button.inline(text="Yes I'm 18+", data="age_verification_true"),
+                Button.inline(text="No I'm Not", data="age_verification_false"),
+            ]
+            markup = event.client.build_reply_markup(buttons)
+            photo = types.InputWebDocument(
+                url="https://i.imgur.com/Zg58iXc.jpg",
+                size=0,
+                mime_type="image/jpeg",
+                attributes=[],
+            )
+            text, msg_entities = await event.client._parse_message_text(
+                "<b>ARE YOU OLD ENOUGH FOR THIS ?</b>", "html"
+            )
+            result = types.InputBotInlineResult(
+                id=str(uuid4()),
+                type="photo",
+                title="Age verification",
+                thumb=photo,
+                content=photo,
+                send_message=types.InputBotInlineMessageMediaAuto(
+                    reply_markup=markup, message=text, entities=msg_entities
+                ),
+            )
+            await event.answer([result] if result else None)
+        elif string == "pmpermit":
+            buttons = [
+                Button.inline(text="اضهار الخيارات.", data="show_pmpermit_options"),
+            ]
+            PM_PIC = gvarstatus("pmpermit_pic")
+            if PM_PIC:
+                CAT = [x for x in PM_PIC.split()]
+                PIC = list(CAT)
+                CAT_IMG = random.choice(PIC)
+            else:
+                CAT_IMG = None
+            query = gvarstatus("pmpermit_text")
+            if CAT_IMG and CAT_IMG.endswith((".jpg", ".jpeg", ".png")):
+                result = builder.photo(
+                    CAT_IMG,
+                    # title="Alive cat",
+                    text=query,
+                    buttons=buttons,
+                )
+            elif CAT_IMG:
+                result = builder.document(
+                    CAT_IMG,
+                    title="Alive cat",
+                    text=query,
+                    buttons=buttons,
+                )
+            else:
+                result = builder.article(
+                    title="Alive cat",
+                    text=query,
+                    buttons=buttons,
+                )
+            await event.answer([result] if result else None)
+    else:
+        buttons = [
+            (
+                Button.url("Source code", "https://github.com/telethontesthelp/Telethon-arabb"),
+                Button.url(
+                    "Deploy",
+                    "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FMr-confused%2Fcatpack&template=https%3A%2F%2Fgithub.com%2FMr-confused%2Fcatpack",
+                ),
+            )
+        ]
+        markup = event.client.build_reply_markup(buttons)
+        photo = types.InputWebDocument(
+            url=CATLOGO, size=0, mime_type="image/jpeg", attributes=[]
+        )
+        text, msg_entities = await event.client._parse_message_text(
+            "iqthon.", "md"
+        )
+        result = types.InputBotInlineResult(
+            id=str(uuid4()),
+            type="photo",
+            title="iqthon",
+            description="نصب لنفسك",
+            url="https://dashboard.heroku.com/new?template=https://github.com/telethon-Arab/teletho-help",
+            thumb=photo,
+            content=photo,
+            send_message=types.InputBotInlineMessageMediaAuto(
+                reply_markup=markup, message=text, entities=msg_entities
+            ),
+        )
+        await event.answer([result] if result else None)
+@iqthon.tgbot.on(CallbackQuery(data=re.compile(b"close")))
+@check_owner
+async def on_plug_in_callback_query_handler(event):
+    buttons = [
+        (Button.inline("Open Menu", data="mainmenu"),),
+    ]
+    await event.edit("Menu Closed", buttons=buttons)
+@iqthon.tgbot.on(CallbackQuery(data=re.compile(rb"mainmenu")))
+@check_owner
+async def on_plug_in_callback_query_handler(event):
+    _result = main_menu()
+    await event.edit(_result[0], buttons=_result[1])
---
+++ userbot/core/logger.py
@@ -0,0 +1,7 @@
+import logging
+
+logging.basicConfig(
+    format="[%(levelname)s- %(asctime)s]- %(name)s- %(message)s- %(filename)s- %(lineno)d",
+    level=logging.INFO,
+    datefmt="الوقت : %I:%M",
+)

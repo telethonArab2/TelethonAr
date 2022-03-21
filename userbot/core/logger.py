3710460e8a2206fe71dbb442f59ca0c842b03dc2
telethonArab <102070450+telethonArab@users.noreply.github.com>
2022-03-21 23:50:50 +0300

    Delete userbot/core directory

--- userbot/core/logger.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-logging.basicConfig(
-    format="[%(levelname)s- %(asctime)s]- %(name)s- %(message)s- %(filename)s- %(lineno)d",
-    level=logging.INFO,
-    datefmt="الوقت : %I:%M",
-)

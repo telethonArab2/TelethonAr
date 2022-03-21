9979f78880f21f994b41edf423e7a0221ccfe349
telethonArab <102070450+telethonArab@users.noreply.github.com>
2022-03-21 23:33:17 +0300

    Create Source.py

---
+++ SQL/extras/Source.py
@@ -0,0 +1,11 @@
+import re
+import time
+from datetime import datetime
+from userbot import StartTime, safeina1
+from userbot.Config import Config
+from userbot.plugins import mention
+help1 = ("**☭⦙ كيفيه التنصيب :**")
+help2 = ("**☭⦙ قـائمـه الاوامـر :**\n**☭⦙ قنـاه السـورس :** @iqthon\n**☭⦙ شـرح اوامـر السـورس : @l3ll3**\n**☭⦙ شـرح فـارات السـورس : @teamtelethon** ")
+TG_BOT = Config.TG_BOT_USERNAME
+TM = time.strftime("%I:%M")
+Sour = f"**.𓄌 : version 7.5  𓇡.** \n.𓄌 : me  {mention}  𓇡. \n**.𓄌 : time  {TM}  𓇡.**\n**.𓄌 : My Bot {TG_BOT} 𓇡.**\n**.𓄌 : Source safeina : @iqthon  𓇡.**"

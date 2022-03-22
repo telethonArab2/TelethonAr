1532b21cedc87206851db33b9d2cb5f2d1da032a
telethonArab <102070450+telethonArab@users.noreply.github.com>
2022-03-22 20:29:42 +0300

    Update الحساب.py

--- userbot/plugins/الحساب.py
+++ userbot/plugins/الحساب.py
@@ -63,6 +63,8 @@ from userbot.helpers.functions import convert_toimage,    deEmojify,    phcommen
 from userbot.helpers.functions import convert_tosticker,    flip_image,    grayscale,    invert_colors,    mirror_file,    solarize
 from ..sql_helper.global_list import add_to_list, get_collection_list, is_in_list, rm_from_list
 from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+from PIL import Image, ImageDraw, ImageFont
+import PIL.ImageOps
 from . import AUTONAME, BOTLOG, BOTLOG_CHATID, DEFAULT_BIO, _catutils, edit_delete, iqthon, logging, spamwatch    
 def inline_mention(user):
     full_name = user_full_name(user) or "No Name"

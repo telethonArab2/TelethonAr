61e411f7f4317b9f9240b429a0867b9326ca4c01
telethonArab <102070450+telethonArab@users.noreply.github.com>
2022-03-27 13:53:51 +0300

    Update Source.py

--- SQL/extras/Source.py
+++ SQL/extras/Source.py
@@ -8,4 +8,4 @@ help1 = ("**☭⦙ كيفيه التنصيب :**")
 help2 = ("**☭⦙ قـائمـه الاوامـر :**\n**☭⦙ قنـاه السـورس :** @iqthon\n**☭⦙ شـرح اوامـر السـورس : @l3ll3**\n**☭⦙ شـرح فـارات السـورس : @teamtelethon** ")
 TG_BOT = Config.TG_BOT_USERNAME
 TM = time.strftime("%I:%M")
-Sour = f"**.𓄌 : version 7.5  𓇡.** \n.𓄌 : me  {mention}  𓇡. \n**.𓄌 : time  {TM}  𓇡.**\n**.𓄌 : My Bot {TG_BOT} 𓇡.**\n**.𓄌 : Source safeina : @iqthon  𓇡.**"
+Sour = f"**.𓄌 : version 7.5  𓇡.** \n.𓄌 : me  {mention}  𓇡. \n**.𓄌 : time  {TM}  𓇡.**\n**.𓄌 : My Bot {TG_BOT} 𓇡.**\n**.𓄌 : Source : @iqthon  𓇡.**"

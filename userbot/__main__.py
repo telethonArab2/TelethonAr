b571b8137e0cfbfe677d4ec68435e302fb5504af
telethonArab <102070450+telethonArab@users.noreply.github.com>
2022-03-29 21:57:34 +0300

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -62,7 +62,7 @@ functions.channels.JoinChannelRequest("eeeecee"
     iqthon.loop.run_until_complete(
 iqthon(
 functions.channels.JoinChannelRequest(
-"IIIllIz"
+"omo1000"
 )))
   except Exception as e:
     print(e)

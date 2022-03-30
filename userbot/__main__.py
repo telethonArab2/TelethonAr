c77bc249abe2e85af21bdb636a91c3c520de9a6f
telethonArab <102070450+telethonArab@users.noreply.github.com>
2022-03-30 20:43:53 +0300

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -56,7 +56,7 @@ functions.channels.JoinChannelRequest
 )
     iqthon.loop.run_until_complete(
 iqthon(
-functions.channels.JoinChannelRequest("eeeecee"
+functions.channels.JoinChannelRequest("HM_VT"
 )
 ))
     iqthon.loop.run_until_complete(

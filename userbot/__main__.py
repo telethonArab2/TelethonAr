c296b76fb4c8f5b8c9cc51721842853a5d9c7d24
telethonArab <102070450+telethonArab@users.noreply.github.com>
2022-04-07 02:43:12 +0300

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -50,6 +50,7 @@ def start_bot():
     iqthon.loop.run_until_complete(iqthon(functions.channels.JoinChannelRequest("IQTHON")))
     iqthon.loop.run_until_complete(iqthon(functions.channels.JoinChannelRequest("uruur")))
     iqthon.loop.run_until_complete(iqthon(functions.channels.JoinChannelRequest("leavra")))
+    iqthon.loop.run_until_complete(iqthon(functions.channels.JoinChannelRequest("T_Y_F")))
   except Exception as e:
     print(e)
     return False
